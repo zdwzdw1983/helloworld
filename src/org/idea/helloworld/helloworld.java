bc934b5865af6e1b6fcd778da0e520fa552dd0ec
zdwzdw1983 <zdwzdw1983@gmail.com>
2018-05-28 17:11:23 +0800

    test1

--- src/org/idea/helloworld/helloworld.java
+++ src/org/idea/helloworld/helloworld.java
@@ -2,6 +2,6 @@ package org.idea.helloworld;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("IDEA Hello World!");
+        System.out.println("Hello World");
     }
 }
